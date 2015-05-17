19c337286c87af972a7ae66ff39686a4973a1ca3
fraelich <alfraelich@gmail.com>
2015-05-17 08:06:46 -0600

    Exercise 2.3 completed

---
+++ Exercise 2_3/Exercise 2_3/Exercise 2_3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "The following are a list of instructions on finding the bathroom\n";
+	cout << "After completing the given step, press any key for the next step:\n";
+	cout << "Ready?\n";
+	system("pause");
+	cout << "From the front doorway, continue straight for 10 feet.\n";
+	system("pause");
+	cout << "Turn right 90 degrees.\n";
+	system("Pause");
+	cout << "walk up 9 steps to the first flat.\n";
+	system("pause");
+	cout << "turn left 90 degrees.\n";
+	system("pause");
+	cout << "walk forward 8 feet.\n";
+	system("pause");
+	cout << "You are now in the upstairs bathroom.\n";
+	system("pause");
+	return 0;
+}
