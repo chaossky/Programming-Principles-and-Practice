fc989e5474d053840dff0ed49b8161680500568c
fraelich <alfraelich@gmail.com>
2015-05-17 08:38:39 -0600

    Example 3.3 completed

---
+++ Exercise 3_3/Exercise 3_3/Exercise 3_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Please enter your first name and age:\n";
+	string first_name;
+	int age;
+	cin >> first_name;
+	cin >> age;
+	cout << "Hello " << first_name << "(age " << age << ")\n";
+
+	system("pause");
+	return 0;
+}
